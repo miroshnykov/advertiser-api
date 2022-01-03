3deb9a30648472fa1388490a8b1f860b82e5453d
miroshnykov <dmytro.miroshnykov@actionmediamtl.com>
2022-01-03 14:19:43 -0500

    auth for offers

--- src/resolvers/offer/OffersResolver.ts
+++ src/resolvers/offer/OffersResolver.ts
@@ -6,10 +6,11 @@ import {
 } from 'type-graphql';
 import { Offer } from '../../entity/Offer';
 import { GqlContext } from '../../types/GqlContext';
+import {auth} from "../../middlewares/auth";
 
 @Resolver()
 export class OffersResolver {
-
+  @UseMiddleware(auth)
   @Query(() => [Offer], { nullable: true })
   async getOffers(
     @Ctx() { payload }: GqlContext,
